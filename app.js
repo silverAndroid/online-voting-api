a787d96d87dc566f4ba8234804c88d1006507135
Rushil Perera <pererarushil1940@gmail.com>
2019-09-11 18:03:30 -0400

    Created .env.example + replaced Sentry with BugSnag

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 require('dotenv').load();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const Sentry = require('@sentry/node');
+const bugsnag = require('@bugsnag/js')
+const bugsnagExpress = require('@bugsnag/plugin-express')
+
+const bugsnagClient = bugsnag(process.env.BUGSNAG_API_KEY)
+bugsnagClient.use(bugsnagExpress)
 
 const canVote = require('./canVote');
 
-const domains = ['http://localhost:3000', 'https://vote.ieeeuottawa.ca'];
+const domains = ['http://localhost:3000', process.env.PROD_URL];
 const app = express();
 const corsOptions = {
     origin: domains,
     optionsSuccessStatus: 200,
 };
 
-Sentry.init({ dsn: process.env.SENTRY_DSN });
-
-app.use(Sentry.Handlers.requestHandler());
+const bugsnagMiddleware = bugsnagClient.getPlugin('express');
+app.use(bugsnagMiddleware.requestHandler);
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
@@ -32,6 +34,6 @@ app.get('/can-vote', (req, res) => canVote(req, res));
 app.use('/users', require('./routes/users_router'));
 app.use('/vote', require('./routes/vote_router'));
 
-app.use(Sentry.Handlers.errorHandler());
+app.use(bugsnagMiddleware.errorHandler);
 
 module.exports = app;
